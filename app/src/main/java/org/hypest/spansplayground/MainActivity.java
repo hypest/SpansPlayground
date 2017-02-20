3eac11d1593fd8ccabf57e833eadbfbf7ef2e488
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2017-02-20 10:19:22 +0200

    Experiment with changing "style" on newline

--- app/src/main/java/org/hypest/spansplayground/MainActivity.java
+++ app/src/main/java/org/hypest/spansplayground/MainActivity.java
@@ -6,7 +6,10 @@ import android.text.Editable;
 import android.text.Spanned;
 import android.text.TextUtils;
 import android.text.TextWatcher;
-import android.text.style.BulletSpan;
+import android.text.style.StrikethroughSpan;
+import android.text.style.SubscriptSpan;
+import android.text.style.SuperscriptSpan;
+import android.text.style.UnderlineSpan;
 import android.util.Log;
 import android.widget.EditText;
 
@@ -26,10 +29,39 @@ public class MainActivity extends Activity {
 
         mEditText = (EditText) findViewById(R.id.edittext);
 
-        mEditText.addTextChangedListener(mTextWatcher);
+        mEditText.setText("Exampletext");
+        mEditText.addTextChangedListener(tw);
+//        mEditText.addTextChangedListener(mTextWatcher);
 
-        mEditText.getText().setSpan(new BulletSpan(), 0, mEditText.getText().length(), Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+        mEditText.getText().setSpan(new SuperscriptSpan(), 0, 7, Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+        mEditText.getText().setSpan(new SubscriptSpan(), 8, mEditText.getText().length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+        mEditText.getText().setSpan(new StrikethroughSpan(), 0, mEditText.getText().length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
     }
+
+    TextWatcher tw = new TextWatcher() {
+        private boolean gotNewline = false;
+        private int position = 0;
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            gotNewline = (count == 1 && s.charAt(start + count - 1) == '\n');
+            position = start;
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            if (gotNewline) {
+                StrikethroughSpan span = mEditText.getText().getSpans(position, position, StrikethroughSpan.class)[0];
+                mEditText.getText().setSpan(span, mEditText.getText().getSpanStart(span), position, Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+                mEditText.getText().setSpan(new UnderlineSpan(), position, position + 1, Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+            }
+        }
+    };
 
     TextWatcher mTextWatcher = new TextWatcher() {
         private String spaces(int count) {

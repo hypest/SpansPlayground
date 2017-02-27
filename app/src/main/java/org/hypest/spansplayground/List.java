088852aff3d427d0d76108d2dba0a4787bb41d0f
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2017-02-27 02:55:55 +0200

    Small code re-org

--- app/src/main/java/org/hypest/spansplayground/List.java
+++ app/src/main/java/org/hypest/spansplayground/List.java
@@ -19,6 +19,14 @@ class List implements TextWatcher {
         text.setSpan(new ListItemSpan(), start, end, Spanned.SPAN_PARAGRAPH);
     }
 
+    private enum PositionType {
+        LIST_START,
+        EMPTY_ITEM_AT_LIST_END,
+        LIST_ITEM_END,
+        TEXT_END,
+        LIST_ITEM_MIDDLE
+    }
+
     private Editable text;
 
     private int inputStart;
@@ -40,8 +48,7 @@ class List implements TextWatcher {
         //  have been changed by other TextWatcher's afterTextChanged calls. This might introduce some inconsistency
         //  and "random" bugs.
 
-        final TextChangedEvent textChangedEvent = new TextChangedEvent(inputStart, charsNew);
-        handleTextChangeForLists(text, textChangedEvent);
+        handleTextChangeForLists(text, new TextChangedEvent(inputStart, charsNew));
     }
 
     private List(Editable text) {
@@ -52,89 +59,116 @@ class List implements TextWatcher {
         newListItem(text, start, end);
     }
 
-    private boolean handleTextChangeForLists(Editable text, TextChangedEvent event) {
+    private void handleTextChangeForLists(Editable text, TextChangedEvent event) {
         // use charsNew to get the spans at the input point. It appears to be more reliable vs the whole Editable.
         ListSpan[] lists = event.charsNew.getSpans(0, 0, ListSpan.class);
         if (lists == null || lists.length == 0) {
             // no list so, nothing to do here
-            return false;
+            return;
         }
 
         SpanWrapper<ListSpan> list = new SpanWrapper<>(text, lists[0]); // TODO: handle nesting
 
         ListItemSpan[] listItems = event.charsNew.getSpans(0, 0, ListItemSpan.class);
-        SpanWrapper<ListItemSpan> listItem = listItems != null && listItems.length > 0 ?
+        SpanWrapper<ListItemSpan> item = listItems != null && listItems.length > 0 ?
                 new SpanWrapper<>(text, listItems[0]) : null;
 
         if (event.gotNewline) {
-            return handleNewlineInList(list, listItem, event.inputStart);
+            switch (getNewlinePositionType(list, item, event.inputStart)) {
+                case LIST_START:
+                    handleNewlineAtListStart(item, event.inputStart);
+                    break;
+                case EMPTY_ITEM_AT_LIST_END:
+                    handleNewlineAtEmptyItemAtListEnd(list, item, event.inputStart);
+                    break;
+                case LIST_ITEM_END:
+                    handleNewlineAtListItemEnd(item, event.inputStart);
+                    break;
+                case TEXT_END:
+                    handleNewlineAtTextEnd();
+                    break;
+                case LIST_ITEM_MIDDLE:
+                    handleNewlineInListItemMiddle(item, event.inputStart);
+                    break;
+            }
         }
 
         if (event.gotEndOfBufferMarker) {
-            return handleEndOfBufferInList(listItem, event.inputStart);
+            handleEndOfBufferInList(item, event.inputStart);
         }
-
-        return false;
     }
 
-    // return true if newline got handled by the list
-    private boolean handleNewlineInList(SpanWrapper<ListSpan> list, SpanWrapper<ListItemSpan> item,
+    private PositionType getNewlinePositionType(SpanWrapper<ListSpan> list, SpanWrapper<ListItemSpan> item,
             int newlineIndex) {
         boolean atEndOfList = newlineIndex == list.getEnd() - 2 || newlineIndex == text.length() - 1;
 
         if (newlineIndex == item.getStart() && !atEndOfList) {
-            // newline added at start of bullet so, add a new bullet
-            newListItem(newlineIndex, newlineIndex + 1);
-
-            // push current bullet forward
-            item.setStart(newlineIndex + 1);
-
-            return true;
+            return PositionType.LIST_START;
         }
 
         if (newlineIndex == item.getStart() && atEndOfList) {
-            // close the list when entering a newline on an empty item at the end of the list
-            item.remove();
-
-            if (list.getEnd() - list.getStart() == 1) {
-                // list only has the empty list item so, remove the list itself as well!
-                list.remove();
-            } else {
-                // adjust the list end to only include the chars before the newline just added
-                list.setEnd(newlineIndex);
-            }
-
-            // delete the newline
-            text.delete(newlineIndex, newlineIndex + 1);
-            return true;
+            return PositionType.EMPTY_ITEM_AT_LIST_END;
         }
 
         if (newlineIndex == item.getEnd() - 2) {
-            // newline added at the end of the bullet. Note: there's already a newline at the bullet end, hence the "-2"
-            //  in the condition instead of "-1".
-
-            // append a new list item span
-            newListItem(newlineIndex + 1, item.getEnd());
-
-            // newline added at the end of the bullet so, adjust the bullet to end at the new newline.
-            item.setEnd(newlineIndex + 1);
-
-            return true;
+            return PositionType.LIST_ITEM_END;
         }
 
         if (newlineIndex == text.length() - 1) {
-            // got a newline while being at the end-of-text. We'll let the current list item engulf it and will wait
-            //  for the end-of-text marker event in order to attach the new list item to it when that happens.
-            return true;
+            return PositionType.TEXT_END;
         }
 
-        {
-            // newline added at some position inside the bullet so, end the current bullet and append a new one
-            newListItem(newlineIndex + 1, item.getEnd());
-            item.setEnd(newlineIndex + 1);
+        // no special case applied so, newline is in the "middle" of the bullet
+        return PositionType.LIST_ITEM_MIDDLE;
+    }
 
-            return true;
+    private void handleNewlineAtListStart(SpanWrapper<ListItemSpan> item, int newlineIndex) {
+        // newline added at start of bullet so, add a new bullet
+        newListItem(newlineIndex, newlineIndex + 1);
+
+        // push current bullet forward
+        item.setStart(newlineIndex + 1);
+    }
+
+    private void handleNewlineAtEmptyItemAtListEnd(SpanWrapper<ListSpan> list, SpanWrapper<ListItemSpan> item,
+            int newlineIndex) {
+        // close the list when entering a newline on an empty item at the end of the list
+        item.remove();
+
+        if (list.getEnd() - list.getStart() == 1) {
+            // list only has the empty list item so, remove the list itself as well!
+            list.remove();
+        } else {
+            // adjust the list end to only include the chars before the newline just added
+            list.setEnd(newlineIndex);
         }
+
+        // delete the newline
+        text.delete(newlineIndex, newlineIndex + 1);
+    }
+
+    private void handleNewlineAtListItemEnd(SpanWrapper<ListItemSpan> item, int newlineIndex) {
+        // newline added at the end of the bullet. Note: there's already a newline at the bullet end, hence the "-2"
+        //  in the condition instead of "-1".
+
+        // append a new list item span
+        newListItem(newlineIndex + 1, item.getEnd());
+
+        // newline added at the end of the bullet so, adjust the bullet to end at the new newline.
+        item.setEnd(newlineIndex + 1);
+    }
+
+    private void handleNewlineAtTextEnd() {
+        // got a newline while being at the end-of-text. We'll let the current list item engulf it and will wait
+        //  for the end-of-text marker event in order to attach the new list item to it when that happens.
+
+        // no-op here
+    }
+
+    private void handleNewlineInListItemMiddle(SpanWrapper<ListItemSpan> item, int newlineIndex) {
+        // newline added at some position inside the bullet so, end the current bullet and append a new one
+        newListItem(newlineIndex + 1, item.getEnd());
+        item.setEnd(newlineIndex + 1);
     }
 
     private boolean handleEndOfBufferInList(SpanWrapper<ListItemSpan> listItem, int markerIndex) {
